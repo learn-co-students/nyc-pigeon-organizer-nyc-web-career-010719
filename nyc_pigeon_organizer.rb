cd936b4eb57bf8192935035d6bfd8b5c6352716a
breezy-karma-0220 <lh62594@yahoo.com>
2018-12-14 17:20:57 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,38 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require 'pry'
+
+def nyc_pigeon_organizer(pigeon_data)
+
+
+pigeon_names = []
+unique_pigeon_names = []
+a_array = []
+a_hash = {}
+pigeon_hash = {}
+
+  pigeon_data.each do |attribute, data|
+    data.each do |key, array|
+      array.each do |names|
+        pigeon_names.push(names)
+      end
+    end
+  end
+
+unique_pigeon_names = pigeon_names.uniq
+
+  unique_pigeon_names.each do |name|
+    pigeon_data.each do |attribute, data|
+      data.each do |key, array|
+        if array.include?(name)
+            a_array.push(key.to_s)
+        end
+      end
+      a_hash[attribute] = a_array
+      a_array = []
+    end
+  pigeon_hash[name] = a_hash
+  a_hash = {}
+  end
+
+return pigeon_hash
+
 end
